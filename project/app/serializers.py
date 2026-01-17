431e40322fe4296f5cc48342cb98cc832c7e8bf6
vaishali <guptavaishali628@gmail.com>
2026-01-17 19:26:49 +0530

    API

--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers  # import serializers after installing 'pip install djangorestframework'
-
+from .models import Student
 class Stu_Serializer(serializers.Serializer):   # syntax same as model, form :
     name=serializers.CharField()
     email=serializers.EmailField()
     city=serializers.CharField()
+    
+    #copy paste DRF code
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
--- project/app/views.py
+++ project/app/views.py
@@ -6,6 +6,7 @@ from rest_framework.renderers import JSONRenderer
 from django.views.decorators.csrf import csrf_exempt
 import io   # for deserializer
 from rest_framework.parsers import JSONParser
+import json
 
 # Create your views here.
 @csrf_exempt
@@ -23,26 +24,104 @@ def stu_list(req):
         py_data=JSONParser().parse(stream)  # convert python data into json data
         print(py_data)
 
-        dserializer = Stu_Serializer(data=py_data)   
-        print(dserializer)
-        print(type(dserializer))
+        serializer = Stu_Serializer(data=py_data)   
+        print(serializer)
+        print(type(serializer))
 
-        if dserializer.is_valid():
-            dserializer.save()
-            return HttpResponse({'msg':'data.save'})
+        if serializer.is_valid():
+            # print(serializer.validated_data)
+            msg={'msg':'data saved'}
+            jsmg_data=JSONRenderer().render(msg)
+            serializer.save()
+            return HttpResponse(jsmg_data , content_type='application/json')
+        
+        else:
+            err_data=JSONRenderer().render(serializer.errors)
+            return HttpResponse(err_data, content_type='application/json')
 
 
     #---------------------------------GET DATA-----------------------------------------
-    # data=Student.objects.all()
-    # serializer=Stu_Serializer(data, many=True) # convert query set data into python by serializer
-    # print(serializer)
-    # print(serializer.data)
-    # json=JSONRenderer().render(serializer.data)  # convert python data into json data
-    # print(json)
+    data=Student.objects.all()
+    serializer=Stu_Serializer(data, many=True) # convert query set data into python by serializer
+    print(serializer)
+    print(serializer.data)
+    json=JSONRenderer().render(serializer.data)  # convert python data into json data
+    print(json)
     
-    # return HttpResponse(json, content_type='application/json')
+    return HttpResponse(json, content_type='application/json')
 
+
+@csrf_exempt
 def stu_detail(req,pk):
+    user = Student.objects.filter(id=pk)
+    if user:
+         #-------------------------------------PUT DATA-----------------------------------------
+
+        if req.method=='PUT':
+            j_data=req.body
+            print(j_data)
+            print(type(j_data))
+            
+            stream=io.BytesIO(j_data)  # jo data a rha h use hum byte io main convert kr rhe hai
+            print(stream)
+            print(type(stream))
+        
+            py_n_data=JSONParser().parse(stream)  # convert python data into json data
+            print(py_n_data)
+            py_o_data = Student.objects.get(id=pk)
+
+            serializer = Stu_Serializer(py_o_data, data=py_n_data)
+            # serializer = Stu_Serializer(py_o_data, data=py_n_data, partial=True)  # PUT act like PATCH if we use partial='True' so we dont need to define PATCH
+            if serializer.is_valid():
+                # print(serializer.validated_data)
+                msg={'msg':'data updated succeessfully'}
+                jsmg_data=JSONRenderer().render(msg)
+                serializer.save()
+                return HttpResponse(jsmg_data , content_type='application/json')
+            
+            else:
+                err_data=JSONRenderer().render(serializer.errors)
+                return HttpResponse(err_data, content_type='application/json')
+
+       #-------------------------------------PATCH DATA-----------------------------------------
+   
+        elif req.method=='PATCH':
+            j_data=req.body
+            print(j_data)
+            print(type(j_data))
+            
+            stream=io.BytesIO(j_data)  # jo data a rha h use hum byte io main convert kr rhe hai
+            print(stream)
+            print(type(stream))
+        
+            py_n_data=JSONParser().parse(stream)  # convert python data into json data
+            print(py_n_data)
+            py_o_data = Student.objects.get(id=pk)
+
+            serializer = Stu_Serializer(py_o_data, data=py_n_data, partial=True)
+            if serializer.is_valid():
+                # print(serializer.validated_data)
+                msg={'msg':'data saved'}
+                jsmg_data=JSONRenderer().render(msg)
+                serializer.save()
+                return HttpResponse(jsmg_data , content_type='application/json')
+            
+            else:
+                err_data=JSONRenderer().render(serializer.errors)
+                return HttpResponse(err_data, content_type='application/json')
+       
+       #-------------------------------------DELETE DATA-----------------------------------------
+
+        elif req.method=="DELETE":
+            py_o_data = Student.objects.get(id=pk)
+            py_o_data.delete()
+            msg={'msg':'data deleted'}
+            jsmg_data=JSONRenderer().render(msg)
+            return HttpResponse(jsmg_data , content_type='application/json')
+    else:
+        msg={'msg':'user not found'}
+        jsmg_data=JSONRenderer().render(msg)
+        return HttpResponse(jsmg_data , content_type='application/json')
     #-------------------------------------GET DATA------------------------------------------
     data=Student.objects.get(id=pk)
     serializer=Stu_Serializer(data) # convert query set data into python by serializer
